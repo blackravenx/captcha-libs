7d6813a175777d33f5af060ba4df5dc1557db252
Max Shyn <makschinasil@gmail.com>
2024-01-28 01:53:15 +0600

    misc(captcha-client)

--- captcha-client/src/lib/captcha-client.ts
+++ captcha-client/src/lib/captcha-client.ts
@@ -1,12 +1,12 @@
 import { type $Fetch } from "ofetch";
 
-abstract class CaptchaClient<TCreateTaskResponse, TGetBalanceResponse = number> {
+abstract class CaptchaClient<TCreateTaskResponse, TaskRequests, TGetBalanceResponse = number> {
   protected abstract clientKey: string;
   protected abstract timeout?: number;
   protected abstract pollingInterval?: number;
   protected abstract httpClient: $Fetch;
   protected abstract createTask(params: unknown): Promise<TCreateTaskResponse>;
-  public abstract solve(params: unknown): Promise<unknown>;
+  public abstract solve(params: TaskRequests): Promise<unknown>;
   protected abstract getBalance(): Promise<TGetBalanceResponse>;
 }
 
